b20e82b57d2c99dd7b436cb47fc4e17603319643
Curtis Elsasser <curt.elsasser@gmail.com>
2021-09-04 19:22:01 -0400

    To support on->off and off->on I am revising our shape enum to include these guys. - including util methods to test high-to-low and on-to-off separately.

--- circles/src/machine.ts
+++ circles/src/machine.ts
@@ -6,7 +6,13 @@
  */
 
 import { IChannel } from '@tiny/core';
-import { CircleShape, ICircleProperties, IParsedInput } from './types';
+import {
+	CircleShape,
+	ICircleProperties,
+	IParsedInput,
+	isShapeHighToLow,
+	isShapeOnToOff,
+} from './types';
 
 /**
  * Processes parsed input and traverses the graph and generates a channel stream
@@ -37,13 +43,14 @@ export class Machine {
 			notes: [],
 			velocities: [],
 		};
+		const shapeIsHighToLow = isShapeHighToLow(circle.shape);
+		const shapeIsOnToOff = isShapeOnToOff(circle.shape);
 		const noteLength = circle.diameter / circle.divisions;
-		const noteDuration = Math.floor(
+		const noteOnDuration = Math.floor(
 			circle.off !== undefined ? noteLength - circle.off : circle.on ?? noteLength
 		);
-		const startingPhase =
-			Math.PI * 2 * circle.phase + (circle.shape === CircleShape.HighToLow ? Math.PI : 0);
-		const caclulateVelocity = (ppq: number): number => {
+		const startingPhase = Math.PI * 2 * circle.phase + (shapeIsHighToLow ? Math.PI : 0);
+		const calculateOnVelocity = (ppq: number): number => {
 			// convert ppq to the position in a cycle (in radians)
 			const offset = (Math.PI * 2 * ppq) / circle.diameter;
 			const normalized = (Math.cos(startingPhase + offset) + 1) / 2;
@@ -55,18 +62,25 @@ export class Machine {
 			ppq < this._input.project.length;
 			index++, ppq += noteLength
 		) {
-			result.durations!.push(noteDuration);
-			result.notes.push(circle.notes);
-			result.velocities!.push(caclulateVelocity(ppq));
 			// see whether we need to introduce some rest time to accommodate for:
 			//  a. noteLength != noteDuration
 			//  b. rational `noteLength`
-			const rest = Math.floor(noteLength * index - noteDuration * index - rested);
+			const rest = Math.floor(noteLength * index - noteOnDuration * index - rested);
+			if (shapeIsOnToOff) {
+				result.durations!.push(noteOnDuration);
+				result.notes.push(circle.notes);
+				result.velocities!.push(calculateOnVelocity(ppq));
+			}
 			if (rest > 0) {
 				result.durations!.push(rest);
 				result.notes.push([]);
 				result.velocities!.push(0);
 				rested += rest;
+			}
+			if (!shapeIsOnToOff) {
+				result.durations!.push(noteOnDuration);
+				result.notes.push(circle.notes);
+				result.velocities!.push(calculateOnVelocity(ppq));
 			}
 		}
 		return result;
